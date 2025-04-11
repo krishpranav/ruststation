6c4a9edb44742aec4a0ceedfd2cb2411d1b5ae5f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-11 18:11:01 +0530

    kernel-framework: macros[derive]

--- kernel-framework/macros/src/derive.rs
+++ kernel-framework/macros/src/derive.rs
@@ -7,24 +7,24 @@ pub fn mapped_kernel(item: ItemStruct) -> syn::Result<TokenStream> {
         Fields::Unnamed(v) if v.unnamed.len() == 1 => v,
         v => {
             return Err(Error::new_spanned(
-               v,
-               "expect a struct with an unnamed field",
+                v,
+                "expect a struct with an unnamed field",
             ));
         }
     };
 
     let ty = match &fields.unnamed[0].ty {
         Type::Ptr(v) => v,
-        v => return Err(Error::new_spanned(v, "expect a struct with a pointer")),,
+        v => return Err(Error::new_spanned(v, "expect a pointer")),
     };
 
     if let Some(t) = ty.mutability {
-        return Err(Error::new_spanned(t, "expect mutable reference"));
+        return Err(Error::new_spanned(t, "expect `const`"));
     }
 
     match ty.elem.as_ref() {
         Type::Path(p) if p.qself.is_none() && p.path.is_ident("u8") => {}
-        v => return Err(Error::new_spanned(v, "expect"))
+        v => return Err(Error::new_spanned(v, "expect `u8`")),
     }
 
     let ident = item.ident;
@@ -37,17 +37,24 @@ pub fn mapped_kernel(item: ItemStruct) -> syn::Result<TokenStream> {
 
                 unsafe {
                     core::arch::asm!(
-                        "rdmsr"
-                    )
-
-                    Self(base as *const u8)
+                        "rdmsr",
+                        in("ecx") 0xc0000082u32,
+                        out("rdx") rdx,
+                        out("rax") rax, 
+                        options(pure, nomem, preserves_flags, nostack)
+                    );
                 }
+
+                let aslr = ((rdx << 32) | rax) - 0xffffffff822001c0;
+                let base = aslr + 0xffffffff82200000;
+
+                Self(base as *const u8)
             }
+        }
 
-            impl rs::MappedKernel for #ident {
-                fn addr(self) -> *const u8 {
-                    self.0
-                }
+        impl okf::MappedKernel for #ident {
+            fn addr(self) -> *const u8 {
+                self.0
             }
         }
     })
