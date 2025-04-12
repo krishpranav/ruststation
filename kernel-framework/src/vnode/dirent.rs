41176f4e238c0cf0791dbe357f2a70eabdd452af
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-12 15:28:26 +0530

    kernel-framework: vnode[dirent]

--- kernel-framework/src/vnode/dirent.rs
+++ kernel-framework/src/vnode/dirent.rs
@@ -1,8 +1,10 @@
 use core::ffi::c_char;
 
 #[repr(C)]
-pub struct DirEntry<const L: usize> {
+pub struct DirEnt<const L: usize> {
     pub id: u32,
     pub len: u16,
+    pub ty: u8,
+    pub name_len: u8,
     pub name: [c_char; L],
 }
