788d918c5487a80a28a096e7e736bb918cc2860e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-13 16:44:36 +0530

    kernel-framework: panic[mod]

--- kernel-framework/src/panic/mod.rs
+++ kernel-framework/src/panic/mod.rs
@@ -1,5 +1,15 @@
-mod msg;
+use self::msg::Message;
 use crate::Kernel;
+use core::fmt::Write;
+use core::panic::PanicInfo;
 
-pub fn panic<K: Kernel>(k: K) -> ! {
+mod msg;
+
+pub fn panic<K: Kernel>(k: K, i: &PanicInfo) -> ! {
+    let mut m = Message::default();
+    let _ = write!(m, "{i}");
+
+    let f = k.get(K::PANIC).as_ptr();
+
+    unsafe { f(c"%s".as_ptr(), m.as_ptr()) };
 }
