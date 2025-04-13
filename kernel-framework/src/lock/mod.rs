bf4d5c0cf144fc74feff9c397be008679028bbcd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-13 18:08:51 +0530

    kernel-framework: lock[mod]

--- kernel-framework/src/lock/mod.rs
+++ kernel-framework/src/lock/mod.rs
@@ -13,7 +13,13 @@ pub struct MtxLock<K: Kernel> {
 
 impl<K: Kernel> MtxLock<K> {
     pub unsafe fn new(kern: K, mtx: *mut K::Mtx) -> Self {
-        unsafe { kern.mtx_lock_flags(mtx, 0, c"".as_ptr(), 0 ) };
+        unsafe { kern.mtx_lock_flags(mtx, 0, c"".as_ptr(), 0) };
         Self { kern, mtx }
+    }
+}
+
+impl<K: Kernel> Drop for MtxLock<K> {
+    fn drop(&mut self) {
+        unsafe { self.kern.mtx_unlock_flags(self.mtx, 0, c"".as_ptr(), 0) };
     }
 }
