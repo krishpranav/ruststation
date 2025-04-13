6959ac5bbe2fac10ecb3887c1177e1eb22dbd3fd
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-13 16:42:33 +0530

    kernel-framework: panic[msg]

---
+++ kernel-framework/src/panic/msg.rs
@@ -0,0 +1,31 @@
+use core::cmp::min;
+use core::ffi::c_char;
+use core::fmt::Write;
+
+pub struct Message {
+    buf: [u8; 2048],
+    pos: usize,
+}
+
+impl Message {
+    pub fn as_ptr(&self) -> *const c_char {
+        self.buf.as_ptr().cast()
+    }
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        Self {
+            buf: [0; 2048],
+            pos: 0,
+        }
+    }
+}
+
+impl Write for Message {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        let len = min(s.len(), (self.buf.len() - 1) - self.pos);
+
+        Ok(())
+    }
+}
