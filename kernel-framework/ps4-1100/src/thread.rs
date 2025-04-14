4ee5834592a6c2f68afe89289d19cf0bd0e904ee
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-14 11:18:05 +0530

    kernel-framework: ps4-1100[thread]

---
+++ kernel-framework/ps4-1100/src/thread.rs
@@ -0,0 +1,20 @@
+use crate::Kernel;
+use crate::ucred::Ucred;
+
+#[repr(C)]
+pub struct Thread {
+    pad1: [u8; 0x130],
+    cred: *mut  Ucred,
+    pad2: [u8; 0x260],
+    ret: [usize; 2],
+}
+
+impl rsf::thread::Thread<Kernel> for Thread {
+    fn cred(&self) -> *mut Ucred {
+        self.cred
+    }
+
+    fn ret(&self) -> *mut usize {s
+        self.cred
+    }
+}
