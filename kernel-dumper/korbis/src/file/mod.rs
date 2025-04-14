25dacb8d8be39940fa38c5853216146a24c1a8d9
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-14 18:21:51 +0530

    kernel-dumper: file[mod]

--- kernel-dumper/korbis/src/file/mod.rs
+++ kernel-dumper/korbis/src/file/mod.rs
@@ -3,10 +3,28 @@ use crate::Kernel;
 use core::sync::atomic::{fence, AtomicU32, Ordering};
 
 pub trait File: Sized {
-    fn refcnt(&self) -> AtomicU32;
+    fn refcnt(&self) -> &AtomicU32;
 }
 
 pub struct OwnedFile<K: Kernel> {
     kernel: K,
     file: *mut K::File,
+}
+
+impl<K: Kernel> OwnedFile<K> {
+    pub unsafe fn new(kernel: K, file: *mut K::File) -> Self {
+        Self { kernel, file }
+    }
+}
+
+impl<K: Kernel> Drop for OwnedFile<K> {
+    fn drop(&mut self) {
+        if unsafe { (*self.file).refcnt().fetch_sub(1, Ordering::Release) } != 1 {
+            return;
+        }
+
+        fence(Ordering::Acquire);
+
+        unsafe { self.kernel.fdrop(self.file, K::Thread::current()) };
+    }
 }
