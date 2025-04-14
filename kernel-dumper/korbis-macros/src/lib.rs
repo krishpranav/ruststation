c2977660183f21ef713f902bb7686927381e5d52
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-14 11:58:52 +0530

    kernel-dumper: korbis-macros[lib]

--- kernel-dumper/korbis-macros/src/lib.rs
+++ kernel-dumper/korbis-macros/src/lib.rs
@@ -1,15 +1,14 @@
-pub mod offset;
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, Error, LitInt};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod offset;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_attribute]
+pub fn offset(args: TokenStream, item: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as LitInt);
+    let item = parse_macro_input!(item as self::offset::OffsetItem);
+
+    self::offset::transform(args, item)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
 }
