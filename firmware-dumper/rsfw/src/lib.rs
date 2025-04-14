28c5f8346a1c1676e0e2cd6063636a19a50a74d4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-14 19:04:53 +0530

    firmware-dumper: rsfw[lib]

--- firmware-dumper/rsfw/src/lib.rs
+++ firmware-dumper/rsfw/src/lib.rs
@@ -1,14 +1,21 @@
 #![no_std]
 
+#[cfg(feature = "read")]
+pub use self::reader::*;
+
 use core::fmt::{Display, Formatter};
 use num_enum::{IntoPrimitive, TryFromPrimitive};
+
+pub mod ps4;
+
+#[cfg(feature = "read")]
+mod reader;
 
 #[cfg(feature = "read")]
 extern crate std;
 
 pub const MAGIC: &[u8; 4] = b"\x7FOBF";
 
-/// Type of top-level item in the dump file.
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, IntoPrimitive, TryFromPrimitive)]
 pub enum DumpItem {
