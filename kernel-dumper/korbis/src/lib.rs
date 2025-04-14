919175f97991d89b7d00962c0075054d6996b75e
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-14 12:01:13 +0530

    kernel-dumper: korbis[lib]

--- kernel-dumper/korbis/src/lib.rs
+++ kernel-dumper/korbis/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+!#[no_std]
+
+pub trait Kernel: Copy + Send + Sync + 'static {
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait KernelExt: Kernel {
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+impl<T: Kernel> KernelExt for T {
 }
