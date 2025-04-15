58d23cff2951bbddfbc49310f46971c5ef76bd8a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-15 17:38:39 +0530

    macros: lib

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,20 +1,61 @@
-pub mod vpath;
-mod enum_conversions;
-mod errno;
+use proc_macro::TokenStream;
+use syn::{Error, ItemEnum, ItemStatic, LitStr, parse_macro_input};
+
 mod bitflag;
 mod elf;
+mod enum_conversions;
+mod errno;
+mod vpath;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[proc_macro_attribute]
+pub fn bitflag(args: TokenStream, item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as ItemEnum);
+    let mut opts = self::bitflag::Options::default();
+    let parser = syn::meta::parser(|m| opts.parse(m));
+
+    parse_macro_input!(args with parser);
+
+    self::bitflag::transform(opts, item)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro_attribute]
+pub fn elf_note(args: TokenStream, item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as ItemStatic);
+    let mut opts = self::elf::Options::default();
+    let parser = syn::meta::parser(|m| opts.parse(m));
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    parse_macro_input!(args with parser);
+
+    self::elf::transform_note(opts, item)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
+
+#[proc_macro_derive(EnumConversions)]
+pub fn implement_conversions(item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as ItemEnum);
+
+    enum_conversions::transform(item)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
+
+#[proc_macro_derive(Errno, attributes(errno))]
+pub fn implement_errno(item: TokenStream) -> TokenStream {
+    let item = parse_macro_input!(item as ItemEnum);
+
+    errno::transform(item)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
+}
+
+#[proc_macro]
+pub fn vpath(arg: TokenStream) -> TokenStream {
+    let arg = parse_macro_input!(arg as LitStr);
+
+    vpath::transform(arg)
+        .unwrap_or_else(Error::into_compile_error)
+        .into()
 }
