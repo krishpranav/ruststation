4049b5a780f828697b50d5deaf653793bd5e03c5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-17 17:19:53 +0530

    config: idps[mod]

--- config/src/idps/mod.rs
+++ config/src/idps/mod.rs
@@ -1,21 +1,54 @@
 #[repr(C)]
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct ConsoleId {
     magic: u16,
+    company: CompanyId,
+    product: ProductId,
+    prodsub: u16,
+    #[cfg_attr(feature = "serde", serde(with = "serde_bytes"))]
+    serial: [u8; 8],
 }
 
 impl ConsoleId {
+    pub fn new(company: CompanyId, product: ProductId, prodsub: u16, serial: [u8; 8]) -> Self {
+        Self {
+            magic: 0,
+            company,
+            product,
+            prodsub,
+            serial,
+        }
+    }
 }
 
 impl Default for ConsoleId {
     fn default() -> Self {
+        Self::new(
+            CompanyId::SONY,
+            ProductId::USA,
+            0x1200,
+            [0x10, 0, 0, 0, 0, 0, 0, 0],
+        )
     }
 }
 
 #[repr(transparent)]
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct CompanyId(u16);
 
 impl CompanyId {
     pub const SONY: Self = Self(0x100);
+}
+
+#[repr(transparent)]
+#[derive(Clone, Copy)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+pub struct ProductId(u16);
+
+impl ProductId {
+    pub const DEVKIT: Self = Self(0x8101);
+    pub const TESTKIT: Self = Self(0x8201);
+    pub const USA: Self = Self(0x8401);
 }
