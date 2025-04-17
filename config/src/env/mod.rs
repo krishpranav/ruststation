2127f821b7f9276cdacfa28b6ee502dfe1a8f6c2
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-17 17:15:50 +0530

    config: mod + lib

---
+++ config/src/env/mod.rs
@@ -0,0 +1,14 @@
+#[repr(C)]
+pub struct PhysMap {
+    pub base: u64,
+    pub len: u64,
+    pub ty: MapType,
+    pub attrs: u32,
+}
+
+#[repr(u32)]
+pub enum MapType {
+    None = 0,
+    Ram = 1,
+    Reserved = 2,
+}
