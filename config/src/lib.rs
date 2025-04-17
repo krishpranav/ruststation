6f6e2a3f51c79d1ff902fc9bf6050118d5773e02
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-17 17:20:27 +0530

    config: lib

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,17 +1,35 @@
+#![no_std]
+
+pub use self::env::*;
+pub use self::idps::*;
+
+use core::num::NonZero;
+
 mod env;
 mod idps;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[repr(C)]
+pub enum BootEnv {
+    Vm(Vm),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[repr(C)]
+#[derive(Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(default))]
+pub struct Config {
+    pub max_cpu: NonZero<usize>,
+    pub idps: ConsoleId,
+    #[cfg_attr(feature = "serde", serde(with = "serde_bytes"))]
+    pub env_vars: [u8; 132096],
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            max_cpu: NonZero::new(1).unwrap(),
+            idps: ConsoleId::default(),
+            env_vars: [0; 132096],
+        }
     }
 }
